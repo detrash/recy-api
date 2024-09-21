62342d5c8e4e4d14a94dc76a7eaae0e00eae598f
zehzo <edson.ferreiraj@hotmail.com>
2024-09-21 17:24:28 -0300

    feat: implementing zod, fix some errors and create tests for audits

--- src/modules/audit/dtos/update-audit.dto.ts
+++ src/modules/audit/dtos/update-audit.dto.ts
@@ -1,7 +1,9 @@
-import { IsOptional, IsString } from 'class-validator';
+import { z } from 'zod';
+import { CreateAuditSchema } from './create-audit.dto';
 
-export class UpdateAuditDto {
-  @IsOptional()
-  @IsString()
-  comments?: string;
-}
+export const UpdateAuditSchema = z.object({
+  audited: z.boolean().optional(),
+  comments: z.string().optional().nullable(),
+});
+
+export type UpdateAuditDto = z.infer<typeof UpdateAuditSchema>;
