98b9794fd745a78bc3c5d1ab21f9492acb82f52f
Yuri Mutti <muttiyuri@gmail.com>
2024-09-23 16:02:10 -0300

    feat: deploy code to ECR

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootdir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
