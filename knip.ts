259d793476ee67db0d51181a0557cf9b62990bf7
Yuri Mutti <muttiyuri@gmail.com>
2024-10-12 17:26:08 -0300

    choro: remove knip

--- knip.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  project: ['src/**/*.ts', 'src/**/*.tsx'],
-  ignore: ['lefthook.yml'],
-  ignoreDependencies: ['ts-node', '@types/express', '@types/multer'],
-  typescript: {
-    config: ['tsconfig.json'],
-  },
-};
