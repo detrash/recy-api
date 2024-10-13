0c10018a03e4dba4c1d1b4e73b30fefd6278ff13
Vinicius Vale <vinicius.oliveira@soufairycode.com>
2024-10-13 15:02:49 -0300

    fix: addition of regex into walletAddres from result dto schema

--- src/modules/calculator/dtos/result.dto.ts
+++ src/modules/calculator/dtos/result.dto.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod';
 
 export const ResultDtoSchema = z.object({
-  walletAddress: z.string({ message: 'walletAddress must be a string' }),
+  walletAddress: z
+    .string()
+    .regex(/^0x[a-fA-F0-9]{40}$/, 'Invalid EVM wallet address format')
+    .or(z.literal(''))
+    .optional(),
   cRecys: z.number({ message: 'cRecys must be a number' }),
   wasteFootprint: z
     .number({ message: 'wasteFootprint must be a number' })
--- src/modules/user/dtos/create-user.dto.ts
+++ src/modules/user/dtos/create-user.dto.ts
@@ -3,10 +3,12 @@ import { z } from 'zod';
 export const CreateUserSchema = z.object({
   email: z
     .string({ message: 'email must be a string' })
-    .email('Please make sure that this is e-mail is valid'),
+    .email({ message: 'Email is required' }),
   name: z.string({ message: 'name must be a string' }),
   phone: z.string({ message: 'phone must be a string' }).optional(),
-  walletAddress: z.string({ message: 'phone must be a string' }).optional(),
+  walletAddress: z
+    .string({ message: 'walletAddress must be a string' })
+    .optional(),
   roleIds: z.array(z.string(), {
     message: 'Role IDs must be an array of strings',
   }),
