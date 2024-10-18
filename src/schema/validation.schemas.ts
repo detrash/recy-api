989ccf238ec8cb1ad4b35fa31339c4387fff5168
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-10-18 18:26:10 +0530

    Update validation.schemas.ts

--- src/schema/validation.schemas.ts
+++ src/schema/validation.schemas.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const phoneSchema = z
   .string()
-  .regex(/^\+?[1-9]\d{1,14}$/, 'Invalid phone number format');
+  .regex(/^(\+|00)?[1-9]\d{1,14}$/, 'Invalid phone number format');
 
 
 export const walletAddressSchema = z
